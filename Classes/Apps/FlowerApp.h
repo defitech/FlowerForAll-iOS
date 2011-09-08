54901ef942098f9092ed2f4a0d00f609d9ddaf46
Perki <pml@defitech.ch>
2011-09-08 12:27:47 +0200

    Moved Files

---
+++ Classes/Apps/FlowerApp.h
@@ -0,0 +1,22 @@
+//
+//  FlowerApp.h
+//  FlowerForAll
+//
+//  Created by Pierre-Mikael Legris on 08.09.11.
+//  Copyright 2011 fondation Defitech All rights reserved.
+//
+
+//  File Naming convention
+//  If you App Name is "MyApp" then all you files should start with "MyApp"
+//  Translations 
+
+#import <Foundation/Foundation.h>
+
+@protocol FlowerApp <UIViewController>
+
+/** Used to put in a button on the App Menu **/
++(UIImage*)AppIcon;
+/** Used to put in as label on the App Menu (Localized)**/
++(NSString*)AppLabel;
+
+@end
