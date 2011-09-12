6b4858e016b15aa3249cd0c7a4deecd112512356
Perki <pml@defitech.ch>
2011-09-12 15:22:40 +0200

    adding Calibration App skeleton

---
+++ Classes/Apps/CalibrationApp/CalibrationApp.h
@@ -0,0 +1,13 @@
+//
+//  CalibrationApp.h
+//  FlowerForAll
+//
+//  Created by Pierre-Mikael Legris on 12.09.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "FlowerApp.h"
+
+@interface CalibrationApp : FlowerApp
+
+@end
