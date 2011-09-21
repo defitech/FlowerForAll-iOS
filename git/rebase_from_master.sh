154d39bad1b9880dac91c69e61242369272b0a48
Perki <pml@defitech.ch>
2011-09-21 14:10:06 +0200

    Added git tools

---
+++ git/rebase_from_master.sh
@@ -0,0 +1,20 @@
+CMD=$(ps aux | grep -c "OS/Xcode")
+if  [ $CMD -gt 1 ];
+then
+  echo "*****Kill Xcode******";
+  exit 
+fi
+
+branch_name=$(git symbolic-ref -q HEAD)
+branch_name=${branch_name##refs/heads/}
+branch_name=${branch_name:-HEAD}
+
+echo "rebase on branch:    $branch_name    ? Y/y"
+read a
+if [[ $a == "Y" || $a == "y" ]]; then
+	echo "Do it"
+        git checkout master; git pull origin master; git checkout $branch_name; git rebase master;
+fi
+
+
+
